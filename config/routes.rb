7305cb4963170384b80597fbaa1cf012e940c089
tchang9 <tlchang@gmail.com>
2019-03-18 18:53:40 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/posts', to: 'posts#index', as: 'posts'
+  get '/posts/new', to: 'posts#new', as: 'new_post'
+  post 'posts', to: 'posts#create'
+  get 'posts/:id', to: 'posts#show', as: 'post'
+  get 'posts/:id/edit', to: 'posts#edit', as: 'edit_post'
+  patch 'posts/:id', to: 'posts#update'
+  delete 'posts/:id', to: 'posts#destroy'
 end
